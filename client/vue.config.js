97b6ab09dea34ebb7378e096e03ed4fb1fb01d63
Ryahn <ryancarr10@gmail.com>
2025-05-21 19:34:04 -0500

    save before adding android, desktop

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'com.discordclone.app',
+        productName: 'Discord Clone',
+        win: {
+          target: ['nsis'],
+          icon: 'public/icon.ico'
+        },
+        linux: {
+          target: ['AppImage', 'deb'],
+          icon: 'public/icon.png'
+        },
+        mac: {
+          target: ['dmg'],
+          icon: 'public/icon.icns'
+        }
+      }
+    }
+  }
 })
